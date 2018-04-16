29df5e2311e31b3e7d4664d076e900bd2c7545a7
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-04-16 21:56:53 +0300

    test ias plan card

--- test_ias.py
+++ test_ias.py
@@ -21,6 +21,14 @@ def test_ias_login(selenium):
     selenium.get('http://inspections.staging.brdo.com.ua/site/login')
     do_login(selenium)
     wait_url(selenium, 'http://inspections.staging.brdo.com.ua/')
+
+
+def enter_code(selenium, edr_code):
+	element = selenium.find_element_by_name('AnnualInspectionPlanned[code]')
+	element.send_keys(edr_code)
+	element = selenium.find_element_by_name('AnnualInspectionPlanned[activity_type]')
+	element.click()
+
     
 
 @pytest.mark.parametrize("edr_code", [
@@ -38,12 +46,28 @@ def test_ias_plan_filter_code(selenium, edr_code):
 	elements = selenium.find_elements_by_css_selector('.table-responsive tbody tr')
 	assert len(elements) > 1
 
-	element = selenium.find_element_by_name('AnnualInspectionPlanned[code]')
-	element.send_keys(edr_code)
-	element = selenium.find_element_by_name('AnnualInspectionPlanned[activity_type]')
-	element.click()
+	enter_code(selenium, edr_code)
 
 	elements = selenium.find_elements_by_css_selector('.table-responsive tbody tr')
 
 	element = elements[0].find_element_by_css_selector('td:nth-child(3) span')
 	assert element.text == edr_code
+
+@pytest.mark.parametrize("edr_code", [
+	'33231317',
+	'34004453',
+	'13318525',
+	'00691748',
+	'40511902',
+])
+def test_ias_plan_card(selenium, edr_code):
+    selenium.get('http://inspections.staging.brdo.com.ua/inspection/planned')
+    do_login(selenium)
+    wait_url(selenium, 'http://inspections.staging.brdo.com.ua/inspection/planned')	
+    enter_code(selenium, edr_code)
+    element = selenium.find_element_by_css_selector('.table_action_btn.icon-details')
+    element.click()
+    assert selenium.current_url.startswith('http://inspections.staging.brdo.com.ua/inspection/view?id=')
+    element = selenium.find_element_by_css_selector('.result_table_mobile_panel tr:nth-child(3) td:nth-child(2)')
+    assert element.text == edr_code
+    
