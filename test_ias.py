5e813c1c659b549e193aa17fd3b8e5cb94d861ff
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-04-18 09:53:09 +0300

    test ias card

--- test_ias.py
+++ test_ias.py
@@ -13,7 +13,7 @@ def do_login(selenium):
 
 def wait_url(selenium, url):
 	assert (selenium.current_url == url or
-		WebDriverWait(selenium, 2).until(EC.url_changes(url)))
+		WebDriverWait(selenium, 10).until(EC.url_changes(url)))
 	assert selenium.current_url == url
 
 
@@ -60,7 +60,7 @@ def test_ias_plan_filter_code(selenium, edr_code):
 	'00691748',
 	'40511902',
 ])
-def test_ias_plan_card(selenium, edr_code):
+def test_ias_plan_card_view(selenium, edr_code):
     selenium.get('http://inspections.staging.brdo.com.ua/inspection/planned')
     do_login(selenium)
     wait_url(selenium, 'http://inspections.staging.brdo.com.ua/inspection/planned')	
@@ -70,4 +70,41 @@ def test_ias_plan_card(selenium, edr_code):
     assert selenium.current_url.startswith('http://inspections.staging.brdo.com.ua/inspection/view?id=')
     element = selenium.find_element_by_css_selector('.result_table_mobile_panel tr:nth-child(3) td:nth-child(2)')
     assert element.text == edr_code
-    
+ 
+def test_ias_plan_card_edit(selenium, edr_code='33231317'):
+	selenium.get('http://inspections.staging.brdo.com.ua/inspection/planned')
+	do_login(selenium)
+	wait_url(selenium, 'http://inspections.staging.brdo.com.ua/inspection/planned')
+	enter_code(selenium, edr_code)
+	element = selenium.find_element_by_css_selector('.table_action_btn.icon-pencil')
+	element.click()
+	assert selenium.current_url.startswith('http://inspections.staging.brdo.com.ua/inspection/update-all?id=')
+	element = selenium.find_element_by_css_selector('.page_title')
+	assert 'Оновити дані про перевірку' in element.text
+
+def test_ias_plan_pdf(selenium):
+    selenium.get('http://inspections.staging.brdo.com.ua/inspection/planned')
+    do_login(selenium)
+    wait_url(selenium, 'http://inspections.staging.brdo.com.ua/inspection/planned')
+    element = selenium.find_element_by_link_text('pdf')
+    element.click()
+    assert selenium.current_url.startswith('https://cdn.inspections.gov.ua/')
+    assert selenium.current_url.endswith('.pdf')
+
+
+@pytest.mark.parametrize("error_code,error_link,error_reason", [
+	('subject_close', 'Суб\'єкт в стадії припинення або припинив діяльність', 'Суб\'єкт господарювання знаходиться в стані припинення діяльності або припинив свою діяльність'),
+])
+def test_ias_plan_error(selenium, error_code, error_link, error_reason):
+    selenium.get('http://inspections.staging.brdo.com.ua/inspection/planned')
+    do_login(selenium)
+    wait_url(selenium, 'http://inspections.staging.brdo.com.ua/inspection/planned')	
+    element = selenium.find_element_by_link_text(error_link)
+    element.click()
+    assert selenium.current_url == 'http://inspections.staging.brdo.com.ua/inspection/planned?AnnualInspectionPlanned%5Berror%5D=' + error_code
+    elements = selenium.find_elements_by_css_selector('.table-responsive tbody tr')
+    assert elements
+    for element in elements:
+    	allert_element = element.find_element_by_css_selector('.icon-alert')
+    	assert allert_element.get_attribute('data-add-tr') == error_reason
+    	assert allert_element.get_attribute('data-original-title') == 'Помилка'
