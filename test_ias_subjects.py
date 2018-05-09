386576dfb2b0bd4c95de19dbd5ce3ef69ce300cd
ayzhanatiulegenova <ayz.tlgnv@gmail.com>
2018-05-09 21:32:02 +0300

    test subject add

--- test_ias_subjects.py
+++ test_ias_subjects.py
@@ -71,3 +71,44 @@ def test_ias_subject_name_many(selenium, name):
     for row_element in elements:
         element = row_element.find_element_by_css_selector('td:nth-child(2)')
         assert name in element.text
+
+
+@pytest.mark.parametrize("subject", [
+    {
+        'Subject[code]': '37580028',
+        'Subject[full_name]': 'КОМУНАЛЬНА УСТАНОВА "МОГИЛІВ-ПОДІЛЬСЬКИЙ РАЙОННИЙ МЕДИЧНИЙ ЦЕНТР ПЕРВИННОЇ МЕДИКО-САНІТАРНОЇ ДОПОМОГИ"',
+        'Subject[short_name]': 'КУ "МОГИЛІВ-ПОДІЛЬСЬКИЙ РМЦПМСД"',
+        'Subject[location]': '24000, Вінницька обл., місто Могилів-Подільський, ВУЛИЦЯ ПОЛТАВСЬКА, будинок 89, корпус 2',
+        'Subject[ceo_name]': 'Цибульчак Євдокія Артемівна',
+        'Subject[status]': 'в стані припинення',
+
+    }
+])
+def test_ias_subject_add(selenium, subject):
+    open_login(selenium, 'http://inspections.staging.brdo.com.ua/subject/index?regulatorId=61')
+    element = selenium.find_element_by_link_text('Додати суб\'єкт господарювання')
+    element.click()
+    assert selenium.current_url == 'http://inspections.staging.brdo.com.ua/subject/add'
+    element = selenium.find_element_by_css_selector('h1.page_title')
+    assert element.text == 'Додати суб’єкт господарювання'
+    assert selenium.title == 'Додати суб\'єкт господарювання'
+    element = selenium.find_element_by_name('SubjectAdd[code]')
+    element.send_keys(subject['Subject[code]'])
+    element = selenium.find_element_by_css_selector('.create_doc_btn')
+    element.click()
+
+    for name, value in subject.items():
+        element = selenium.find_element_by_name(name)
+        assert element.get_attribute('value') == value
+
+def test_ias_subject_add_error(selenium):   
+    open_login(selenium,'http://inspections.staging.brdo.com.ua/subject/add')
+    element = selenium.find_element_by_name('SubjectAdd[code]')
+    element.send_keys('3758002')
+    element = selenium.find_element_by_css_selector('.create_doc_btn')
+    element.click()
+    element = selenium.find_element_by_css_selector('.field-subjectadd-code')
+    assert 'has-error' in element.get_attribute('class')
+    element = element.find_element_by_css_selector('.help-block')
+    assert element.text == 'Значення "Код суб\'єкту господарювання" повинно містити мінімум 8 символів.'
+
